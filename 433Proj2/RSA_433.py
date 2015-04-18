0c756cc8b037b3a4583fe584cf2489de5235e38d
dvalbuena <denice_valbuena@csu.fullerton.edu>
2015-04-18 21:59:26 +0000

    fixed RSA

--- 433Proj2/RSA_433.py
+++ 433Proj2/RSA_433.py
@@ -6,43 +6,25 @@ from base64 import b64decode
 class RSA_433(Cipher):
     def setKey(self, key):
         try:
-            self.key = key
-        
-            key = open(self.key, "r").read()
+            self.key = open(key, "r").read()
             
             return True
         except ValueError:
             return False
 
-    def encrypt(key, plaintext):
-        '''ey = open(public_key_loc, "r").read()
-        rsakey = RSA.importKey(key)
-        rsakey = PKCS1_OAEP.new(rsakey)
-        encrypted = rsakey.encrypt(message)
-        return encrypted.encode('base64')'''
-        
-        #key = open(self.key, "r").read()
-        rsakey = RSA.importKey(key)
+    def encrypt(self, plaintext):
+
+        rsakey = RSA.importKey(self.key)
         rsakey = PKCS1_OAEP.new(rsakey)
         encrypted = rsakey.encrypt(plaintext)
-        return ''.join(encrypted.encode('base64'))
+        return encrypted.encode('base64')
         
 
-    def decrypt(key, ciphertext):
-        '''from Crypto.PublicKey import RSA 
-        from Crypto.Cipher import PKCS1_OAEP 
-        from base64 import b64decode 
-        key = open(private_key_loc, "r").read() 
-        rsakey = RSA.importKey(key) 
+    def decrypt(self, ciphertext):
+ 
+        rsakey = RSA.importKey(self.key) 
         rsakey = PKCS1_OAEP.new(rsakey) 
-        decrypted = rsakey.decrypt(b64decode(package)) 
-        return decrypted'''
-        
-        
-        #key2 = open("privkey.pem", "r").read() 
-        rsakey = RSA.importKey(key) 
-        rsakey = PKCS1_OAEP.new(rsakey) 
-        decrypted = rsakey2.decrypt(b64decode(ciphertext)) 
-        return ''.join(decrypted)
+        decrypted = rsakey.decrypt(b64decode(ciphertext)) 
+        return decrypted
         #plaintext = 'NOT CODED'
         #return plaintext
